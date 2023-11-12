3462cfae20d113b8794cc63dc77b50c43a879564
victorsales170903 <victorsales@ufba.br>
2023-11-12 00:03:37 -0300

    Alterando cores dos tipos

--- pokedex/src/components/pokemon.tsx
+++ pokedex/src/components/pokemon.tsx
@@ -1,27 +1,42 @@
-import { PokemonUnique } from "@/types/poke"
+// pokemon.tsx
 
-export default function Pokemon ({name, sprites, types}: PokemonUnique){
-    return(
-        <div className="pokemon">
-            <div className="info">
-                {/* nome do pokemon */}
-                <p className="pokemon-name"> {name} </p>
-                
-                {/* tipos do pokemon */}
-                {types.map((type, index : number) => {
-                    return(
-                        <div className="pokemon-types" key={index}>
-                        {type.type.name}
-                        </div>
-                        
-                    )
-                })}
-            </div>
+import React from 'react';
+import { PokemonUnique } from '@/types/poke';
 
-            {/* imagem do pokemon*/}
-            <div className="pokemon-sprites">
-                <img src={sprites.front_default} alt= {name} />
-            </div>
-        </div>
-    )
+interface PokemonProps {
+  name: string;
+  sprites: { front_default: string };
+  types: { type: { name: string } }[];
 }
+
+const Pokemon: React.FC<PokemonProps> = ({ name, sprites, types }) => {
+  const typeColors: Record<string, string> = {
+    grass: '#7AC74C',
+    fire: '#EE8130',
+    water: '#9cc1ed',
+    electric: '#ffff6c',
+  };
+
+  return (
+    <div className="pokemon">
+      <div className="info">
+        {/* nome do pokemon */}
+        <p className="pokemon-name"> {name} </p>
+
+        {/* tipos do pokemon */}
+        {types.map((type, index: number) => (
+          <div className="pokemon-types" key={index} style={{ backgroundColor: typeColors[types[0].type.name] || 'gray' }}>
+            {type.type.name}
+          </div>
+        ))}
+      </div>
+
+      {/* imagem do pokemon*/}
+      <div className="pokemon-sprites">
+        <img src={sprites.front_default} alt={name} />
+      </div>
+    </div>
+  );
+};
+
+export default Pokemon;
