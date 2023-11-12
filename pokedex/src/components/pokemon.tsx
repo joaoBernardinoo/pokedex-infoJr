b2caf29d83ef2fcbfb45433eea9ee60b77bb5b5d
victorsales170903 <victorsales@ufba.br>
2023-11-12 15:44:43 -0300

    Atualizações

--- pokedex/src/components/pokemon.tsx
+++ pokedex/src/components/pokemon.tsx
@@ -1,6 +1,30 @@
+// pokemon.tsx
 import { PokemonUnique } from "@/types/poke"
+import React, {useContext} from "react";
+import FavoriteContext from "../contexts/favoritesContext";
 
-export default function Pokemon ({name, sprites, types}: PokemonUnique){
+interface PokemonProps {
+    name: string;
+    sprites: { front_default: string };
+    types: { type: { name: string } }[];
+  }
+
+
+export default function Pokemon ({id, name, sprites, types}: PokemonUnique){
+    const {favoritePokemons, updateFavoritePokemons} = useContext(FavoriteContext);
+    
+    const heart = favoritePokemons.includes(id) ? "❤️" : "🖤";
+    const onHeartClick = () =>{
+        updateFavoritePokemons(id);
+    }
+
+    const typeColors: Record<string, string> = {
+        grass: '#7AC74C',
+        fire: '#EE8130',
+        water: '#9cc1ed',
+        electric: '#ffff6c',
+      };
+    
     return(
         <div className="pokemon">
             <div className="info">
@@ -10,7 +34,7 @@ export default function Pokemon ({name, sprites, types}: PokemonUnique){
                 {/* tipos do pokemon */}
                 {types.map((type, index : number) => {
                     return(
-                        <div className="pokemon-types" key={index}>
+                        <div className="pokemon-types" key={index} style={{ backgroundColor: typeColors[types[0].type.name] || 'gray' }}>
                         {type.type.name}
                         </div>
                         
@@ -22,6 +46,11 @@ export default function Pokemon ({name, sprites, types}: PokemonUnique){
             <div className="pokemon-sprites">
                 <img src={sprites.front_default} alt= {name} />
             </div>
+
+            {/* botão de favoritar */}
+            <button onClick={onHeartClick}>
+                {heart}
+            </button>
         </div>
     )
 }
