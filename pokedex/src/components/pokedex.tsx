745af0b03d712ebefb119ebb0d360a10b81e9e58
joaoBernardinoo <joaobernardino@ufba.br>
2023-11-12 17:28:29 -0300

    .

--- pokedex/src/components/pokedex.tsx
+++ pokedex/src/components/pokedex.tsx
@@ -1,7 +1,7 @@
 import Searchbar from '@/components/Searchbar';
 import { getPokemonData, getPokemons } from '@/pages/api/pokemonAPI';
 import { useEffect, useState } from 'react';
-import Pokemon from './Pokemon';
+import Pokemon from './pokemon';
 import Pokelogo from '@/images/pokelogo.png';
 import Image from 'next/image';
 import { PokemonAll, PokemonUnique } from '@/types/poke';
