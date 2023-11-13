94200ffaea30bbd9476f616c17dd7df656426485
joaoBernardinoo <joaobernardino@ufba.br>
2023-11-13 17:44:03 -0300

    feat: infinite rolling

--- pokedex/src/components/pokedex.tsx
+++ pokedex/src/components/pokedex.tsx
@@ -1,41 +1,66 @@
 import Searchbar from '@/components/Searchbar';
 import { getPokemonData, getPokemons } from '@/pages/api/pokemonAPI';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Pokemon from './pokemon';
 import Pokelogo from '@/images/pokelogo.png';
 import Image from 'next/image';
 import { PokemonAll, PokemonUnique } from '@/types/poke';
 import React, { useContext } from 'react';
 import FavoriteContext from '../contexts/favoritesContext';
-
+import { off } from 'process';
 
 export default function Pokedex() {
   // lista com todos os pokemons da página inicial
   const [pokemons, setPokemons] = useState<PokemonUnique[]>([]);
   const [notFound, setNotFound] = useState(false);
+  const [visiblePokemons, setVisiblePokemons] = useState<PokemonUnique[]>([]);
   const { favoritePokemons } = useContext(FavoriteContext);
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
-  // ---------------------------------
-  // Funções
-  
-  // pega as informações da API
-  const fetchPokemons = async () => {
+  const fetchPokemons = async (limit = 9, offset = 0) => {
     try {
-      // pega todos os pokemons para a página inicial
-      const result = await getPokemons();
-
-      // pega cada pokemon e extrai o objeto individual dele pela api (contém os atributos do pokemon)
+      const result = await getPokemons(limit, offset);
       const promises = result.map(async (pokemon: PokemonAll) => {
         const pokeData = await getPokemonData(pokemon.url);
         return pokeData;
       });
 
-      // espera todas as requisições serem feitas para formar a lista
-      const data = await Promise.all(promises);
+      const data: PokemonUnique[] = await Promise.all(promises);
       setPokemons(data);
+      // Define uma quantidade inicial de Pokémon a serem exibidos
+      setVisiblePokemons(data.slice(0, 9));
     } catch (error) {
       console.log('fetch error: ', error);
     }
+  };
+
+  const fetchMorePokemons = async (offset: number) => {
+    try {
+      const result = await getPokemons(9, offset);
+      const promises = result.map(async (pokemon: PokemonAll) => {
+        const pokeData = await getPokemonData(pokemon.url);
+        return pokeData;
+      });
+
+      const data: PokemonUnique[] = await Promise.all(promises);
+      return data;
+    } catch (error) {
+      console.log('fetch error: ', error);
+    }
+  };
+
+  const loadMorePokemons = async () => {
+    const currentLength = visiblePokemons.length;
+    const totalLength = pokemons.length;
+    
+    if (totalLength + 9 >= currentLength) {
+      const newData = await fetchMorePokemons(totalLength)
+      if (newData)
+      setPokemons([...pokemons,...newData]);
+    }
+    const nextPokemons = pokemons.slice(currentLength, currentLength + 9);
+    setVisiblePokemons([...visiblePokemons, ...nextPokemons]);
+
   };
 
   // altera ambiente do site de acordo com o que é digitado
@@ -46,55 +71,71 @@ export default function Pokedex() {
     }
 
     setNotFound(false);
-    
+
     const result = pokemons.filter((pokemon) => {
       return pokemon.name.toLowerCase().includes(name.toLowerCase());
     });
 
-    if (result.length == 0) {
+    if (result.length === 0) {
       setNotFound(true);
     } else {
       setPokemons(result);
+      setVisiblePokemons(result.slice(0, 10));
     }
   };
 
   const filterFavorites = () => {
     const result = pokemons.filter((pokemon) => favoritePokemons.includes(pokemon.id));
     setPokemons(result);
-  }
+    setVisiblePokemons(result.slice(0, 50));
+  };
 
   // ---------------------------------------
   // Efeitos dinâmicos
+  const handleScroll = () => {
+    const containerHeight = containerRef.current?.clientHeight;
+    const scrollTop = 420 + document.documentElement.scrollTop;
+    if (containerHeight)
+      if (scrollTop >= containerHeight) {
+        loadMorePokemons();
+      }
+  };
   useEffect(() => {
-    fetchPokemons();
+    fetchPokemons(9);
   }, []);
 
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [visiblePokemons]);
 
   return (
     <div className="body">
-      <div className='title'>
-        <Image
-          src={Pokelogo}
-          alt="pokelogo"
-          style={{ display: 'block' }}
-        />
+      <div className="title">
+        <Image src={Pokelogo} alt="pokelogo" style={{ display: 'block' }} />
         <h1>Pokédex</h1>
       </div>
 
       <Searchbar onSearch={onSearchHandler} />
-
-      <div className='filters'>
+      <div className="filters">
         <h3>Filtro:</h3>
-        <button onClick={fetchPokemons} className='fetchPokemons'>Todos os Pokemons</button>
-        <button onClick={filterFavorites} className='filterFavorites'>Apenas os favoritos</button>
+        <button onClick={fetchPokemons} className="fetchPokemons">
+          Todos os Pokemons
+        </button>
+        <button onClick={filterFavorites} className="filterFavorites">
+          Apenas os favoritos
+        </button>
       </div>
 
       {notFound ? (
         <h2>Pokemon não encontrado! Você digitou o nome certo?</h2>
       ) : (
-        <div className="pokemons">
+        <div className="pokemons" ref={containerRef}>
           {/* retorna lista de pokemons da página inicial */}
-          {pokemons.map((pokemon) => {
+          {visiblePokemons.map((pokemon) => {
             return (
               <Pokemon
                 key={pokemon.id}
@@ -108,7 +149,6 @@ export default function Pokedex() {
           })}
         </div>
       )}
-
     </div>
   );
 }
--- pokedex/src/pages/api/pokemonAPI.ts
+++ pokedex/src/pages/api/pokemonAPI.ts
@@ -1,23 +1,22 @@
-export const getPokemons = async (limit = 50) => {
-    try {
-        let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offsset=1`
-        
-        const res = await fetch(url);
-        const data = await res.json();
-        
-        return data.results;
-    } catch (error) {
-        console.log("error: ", error);
-    }
-}
+export const getPokemons = async (limit = 50, offset = 0) => {
+  try {
+    let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data.results;
+  } catch (error) {
+    console.log('error: ', error);
+  }
+};
 
 // usar a url na array do getPokemons para pegar o dado individual
 export const getPokemonData = async (url: string) => {
-    try {  
-        const res = await fetch(url);
-        return await res.json();
-    } catch (error) {
-        console.log("error: ", error);
-    }
-}
-
+  try {
+    const res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log('error: ', error);
+  }
+};
