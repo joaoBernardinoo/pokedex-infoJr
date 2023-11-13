d10bfbadcbd0cf7f459a1873674a08e0a7f9fd79
Caio Viana <caiohebertv@gmail.com>
2023-11-13 15:35:55 -0300

    responsividade

--- pokedex/src/components/auth.tsx
+++ pokedex/src/components/auth.tsx
@@ -6,8 +6,8 @@ export default function LoginModal({ session }: LoginModalProps) {
   if (session) {
     return (
       <>
-        <h3>Seja bem-vindo {session.nickname}</h3>
-        <a href="/api/auth/logout" className="login">Deslogar</a>
+        <h3>Seja bem-vindo, {session.nickname}!</h3>
+        <a href="/api/auth/logout" className="logout">Deslogar</a>
       </>
     );
   }
